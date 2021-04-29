efbd0256b330703e6e8cf976ebd53f7c8989ed56
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-30 03:24:49 +0800

    update Navbar

--- src/components/molecules/Navbar/index.js
+++ src/components/molecules/Navbar/index.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light shadow-sm">
       <div className="container-fluid">
         <Link className="navbar-brand" to="/">
           S11720037
