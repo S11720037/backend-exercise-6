c1b0258f5affd6c7f201ce660011f7453eb9a1e0
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-30 04:10:21 +0800

    finised adding data

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,21 +1,150 @@
+import { useState, useEffect } from "react";
+
+import { Spinner } from "../../components";
+import { firebase } from "../../config";
+
 function Home() {
+  const [productName, setProductName] = useState("");
+  const [category, setCategory] = useState("");
+  const [price, setPrice] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    firebase
+      .database()
+      .ref("products")
+      .on("value", res => {
+        if (res.val()) {
+          const rawData = res.val();
+          const productArray = [];
+          Object.keys(rawData).map(i => {
+            productArray.push({ id: i, ...rawData[i] });
+          });
+          setProducts(productArray);
+        }
+      });
+  }, []);
+
+  const resetData = () => {
+    setProductName("");
+    setCategory("");
+    setPrice("");
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    setIsLoading(true);
+
+    const data = {
+      product: productName,
+      category: category,
+      price: price,
+    };
+
+    firebase
+      .database()
+      .ref("products")
+      .push(data)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+
+    resetData();
+
+    setIsLoading(false);
+  };
+
   return (
     <div>
       <h2 className="text-center">Home</h2>
       <hr />
-      <p>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
-        ducimus minus laudantium voluptate laborum fugit atque. Labore amet iure
-        aut quo nihil ad sint excepturi tempore totam quia ut dolorem
-        repudiandae autem quasi porro, ex quam recusandae est alias sunt sequi
-        voluptatibus laudantium cumque? Ducimus, magni expedita quod nulla
-        voluptatem, voluptatum exercitationem ipsum at deleniti rerum animi
-        dolor veniam officiis praesentium dolorum? Tempore eveniet corporis
-        provident quaerat dolor, maxime velit cumque ad blanditiis qui vitae
-        facilis, nemo aliquam vel consectetur! Accusamus eum rerum quisquam
-        reiciendis quae temporibus voluptates molestias veniam inventore
-        maiores. Cupiditate impedit similique ipsa earum, enim harum eum?
-      </p>
+      <form>
+        <div className="mb-3">
+          <label htmlFor="product-name" className="form-label">
+            Product Name
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="product-name"
+            placeholder="Type the product name"
+            value={productName}
+            onChange={e => setProductName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="category" className="form-label">
+            Category
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="category"
+            placeholder="Type the category"
+            value={category}
+            onChange={e => setCategory(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="price" className="form-label">
+            Price
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="price"
+            placeholder="Type the product name"
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+          />
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            style={{ minWidth: "150px" }}
+            onClick={e => onSubmit(e)}
+          >
+            {isLoading && <Spinner />}
+            {!isLoading && "Save"}
+          </button>
+        </div>
+      </form>
+      <hr />
+      <table className="table table-responsive">
+        <thead>
+          <tr>
+            <th scope="col">Product Name</th>
+            <th scope="col">Category</th>
+            <th scope="col">Price</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map(i => {
+            return (
+              <tr key={i.id}>
+                <td>{i.product}</td>
+                <td>{i.category}</td>
+                <td>{i.price}</td>
+                <td>
+                  <button type="button" className="btn btn-success btn-sm m-1">
+                    Edit
+                  </button>
+                  <button type="button" className="btn btn-danger btn-sm m-1">
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
     </div>
   );
 }
