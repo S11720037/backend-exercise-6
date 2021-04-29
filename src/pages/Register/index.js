ce15517c923904bf80420dd0ae344f1ae0fe7a4a
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-30 03:15:47 +0800

    update Register page

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -38,55 +38,59 @@ function Register() {
   };
 
   return (
-    <form>
-      <div className="mb-3">
-        <label htmlFor="nama-lengkap" className="form-label">
-          Nama Lengkap
-        </label>
-        <input
-          type="text"
-          className="form-control"
-          id="nama-lengkap"
-          value={fullName}
-          onChange={e => setFullName(e.target.value)}
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="email" className="form-label">
-          Email
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="email"
-          value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="mb-3">
-        <label htmlFor="password" className="form-label">
-          Password
-        </label>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-      <div className="text-center">
-        <button
-          type="submit"
-          className="btn btn-primary"
-          onClick={e => onSubmit(e)}
-          style={{ minWidth: "150px" }}
-        >
-          {isLoading && <Spinner />}
-          {!isLoading && "Daftar"}
-        </button>
-      </div>
-    </form>
+    <div>
+      <h2 className="text-center">Register</h2>
+      <hr />
+      <form>
+        <div className="mb-3">
+          <label htmlFor="nama-lengkap" className="form-label">
+            Nama Lengkap
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="nama-lengkap"
+            value={fullName}
+            onChange={e => setFullName(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="email" className="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            value={email}
+            onChange={e => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <div className="text-center">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            onClick={e => onSubmit(e)}
+            style={{ minWidth: "150px" }}
+          >
+            {isLoading && <Spinner />}
+            {!isLoading && "Daftar"}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 }
 
