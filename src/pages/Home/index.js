bb639c738ce2ffc00c45956d712ce3342367c729
Arter Tendean <arter.tendean.07@gmail.com>
2021-04-30 03:18:02 +0800

    adding Home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,23 @@
+function Home() {
+  return (
+    <div>
+      <h2 className="text-center">Home</h2>
+      <hr />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Blanditiis
+        ducimus minus laudantium voluptate laborum fugit atque. Labore amet iure
+        aut quo nihil ad sint excepturi tempore totam quia ut dolorem
+        repudiandae autem quasi porro, ex quam recusandae est alias sunt sequi
+        voluptatibus laudantium cumque? Ducimus, magni expedita quod nulla
+        voluptatem, voluptatum exercitationem ipsum at deleniti rerum animi
+        dolor veniam officiis praesentium dolorum? Tempore eveniet corporis
+        provident quaerat dolor, maxime velit cumque ad blanditiis qui vitae
+        facilis, nemo aliquam vel consectetur! Accusamus eum rerum quisquam
+        reiciendis quae temporibus voluptates molestias veniam inventore
+        maiores. Cupiditate impedit similique ipsa earum, enim harum eum?
+      </p>
+    </div>
+  );
+}
+
+export default Home;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,19 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import { Register, Login } from "../pages";
+import { Register, Login, Home } from "../pages";
 
 function Routes() {
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <Register />
+          <Home />
         </Route>
         <Route path="/login">
           <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
         </Route>
       </Switch>
     </Router>
